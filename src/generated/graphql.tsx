57d15c3ba6c3a3b22ee39d3a7ea1a1d805114096
lmjcbs <hello@liamjacobs.dev>
2020-08-25 07:32:12 +0100

    add current user query

--- src/generated/graphql.tsx
+++ src/generated/graphql.tsx
@@ -1,134 +1,202 @@
 import gql from 'graphql-tag';
 import * as Urql from 'urql';
 export type Maybe<T> = T | null;
-export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type Exact<T extends { [key: string]: unknown }> = {
+    [K in keyof T]: T[K];
+};
 export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
-  ID: string;
-  String: string;
-  Boolean: boolean;
-  Int: number;
-  Float: number;
+    ID: string;
+    String: string;
+    Boolean: boolean;
+    Int: number;
+    Float: number;
 };
 
 export type Query = {
-  __typename?: 'Query';
-  users: Array<User>;
-  currentUser?: Maybe<User>;
-  leagues: Array<League>;
-  league?: Maybe<League>;
+    __typename?: 'Query';
+    users: Array<User>;
+    currentUser?: Maybe<User>;
+    leagues: Array<League>;
+    league?: Maybe<League>;
 };
 
-
 export type QueryLeagueArgs = {
-  id: Scalars['Float'];
+    id: Scalars['Float'];
 };
 
 export type User = {
-  __typename?: 'User';
-  id: Scalars['Float'];
-  createdAt: Scalars['String'];
-  updatedAt: Scalars['String'];
-  username: Scalars['String'];
+    __typename?: 'User';
+    id: Scalars['Float'];
+    createdAt: Scalars['String'];
+    updatedAt: Scalars['String'];
+    username: Scalars['String'];
 };
 
 export type League = {
-  __typename?: 'League';
-  id: Scalars['Float'];
-  createdAt: Scalars['String'];
-  updatedAt: Scalars['String'];
-  name: Scalars['String'];
+    __typename?: 'League';
+    id: Scalars['Float'];
+    createdAt: Scalars['String'];
+    updatedAt: Scalars['String'];
+    name: Scalars['String'];
 };
 
 export type Mutation = {
-  __typename?: 'Mutation';
-  register: UserResponse;
-  login: UserResponse;
-  createLeague: League;
-  updateLeague?: Maybe<League>;
-  deleteLeague: Scalars['Boolean'];
+    __typename?: 'Mutation';
+    register: UserResponse;
+    login: UserResponse;
+    createLeague: League;
+    updateLeague?: Maybe<League>;
+    deleteLeague: Scalars['Boolean'];
 };
-
 
 export type MutationRegisterArgs = {
-  options: UsernamePasswordInput;
+    options: UsernamePasswordInput;
 };
-
 
 export type MutationLoginArgs = {
-  options: UsernamePasswordInput;
+    options: UsernamePasswordInput;
 };
-
 
 export type MutationCreateLeagueArgs = {
-  name: Scalars['String'];
+    name: Scalars['String'];
 };
-
 
 export type MutationUpdateLeagueArgs = {
-  name?: Maybe<Scalars['String']>;
-  id: Scalars['Float'];
+    name?: Maybe<Scalars['String']>;
+    id: Scalars['Float'];
 };
 
-
 export type MutationDeleteLeagueArgs = {
-  id: Scalars['Float'];
+    id: Scalars['Float'];
 };
 
 export type UserResponse = {
-  __typename?: 'UserResponse';
-  errors?: Maybe<Array<FieldError>>;
-  user?: Maybe<User>;
+    __typename?: 'UserResponse';
+    errors?: Maybe<Array<FieldError>>;
+    user?: Maybe<User>;
 };
 
 export type FieldError = {
-  __typename?: 'FieldError';
-  field: Scalars['String'];
-  message: Scalars['String'];
+    __typename?: 'FieldError';
+    field: Scalars['String'];
+    message: Scalars['String'];
 };
 
 export type UsernamePasswordInput = {
-  username: Scalars['String'];
-  password: Scalars['String'];
+    username: Scalars['String'];
+    password: Scalars['String'];
+};
+
+export type StandardUserFragment = { __typename?: 'User' } & Pick<
+    User,
+    'id' | 'username'
+>;
+
+export type LoginMutationVariables = Exact<{
+    options: UsernamePasswordInput;
+}>;
+
+export type LoginMutation = { __typename?: 'Mutation' } & {
+    login: { __typename?: 'UserResponse' } & {
+        errors?: Maybe<
+            Array<
+                { __typename?: 'FieldError' } & Pick<
+                    FieldError,
+                    'field' | 'message'
+                >
+            >
+        >;
+        user?: Maybe<{ __typename?: 'User' } & StandardUserFragment>;
+    };
 };
 
 export type RegisterMutationVariables = Exact<{
-  username: Scalars['String'];
-  password: Scalars['String'];
+    username: Scalars['String'];
+    password: Scalars['String'];
 }>;
 
+export type RegisterMutation = { __typename?: 'Mutation' } & {
+    register: { __typename?: 'UserResponse' } & {
+        errors?: Maybe<
+            Array<
+                { __typename?: 'FieldError' } & Pick<
+                    FieldError,
+                    'field' | 'message'
+                >
+            >
+        >;
+        user?: Maybe<{ __typename?: 'User' } & StandardUserFragment>;
+    };
+};
 
-export type RegisterMutation = (
-  { __typename?: 'Mutation' }
-  & { register: (
-    { __typename?: 'UserResponse' }
-    & { errors?: Maybe<Array<(
-      { __typename?: 'FieldError' }
-      & Pick<FieldError, 'field' | 'message'>
-    )>>, user?: Maybe<(
-      { __typename?: 'User' }
-      & Pick<User, 'id' | 'username'>
-    )> }
-  ) }
-);
+export type CurrentUserQueryVariables = Exact<{ [key: string]: never }>;
 
+export type CurrentUserQuery = { __typename?: 'Query' } & {
+    currentUser?: Maybe<{ __typename?: 'User' } & StandardUserFragment>;
+};
 
+export const StandardUserFragmentDoc = gql`
+    fragment StandardUser on User {
+        id
+        username
+    }
+`;
+export const LoginDocument = gql`
+    mutation Login($options: UsernamePasswordInput!) {
+        login(options: $options) {
+            errors {
+                field
+                message
+            }
+            user {
+                ...StandardUser
+            }
+        }
+    }
+    ${StandardUserFragmentDoc}
+`;
+
+export function useLoginMutation() {
+    return Urql.useMutation<LoginMutation, LoginMutationVariables>(
+        LoginDocument
+    );
+}
 export const RegisterDocument = gql`
     mutation Register($username: String!, $password: String!) {
-  register(options: {username: $username, password: $password}) {
-    errors {
-      field
-      message
+        register(options: { username: $username, password: $password }) {
+            errors {
+                field
+                message
+            }
+            user {
+                ...StandardUser
+            }
+        }
     }
-    user {
-      id
-      username
-    }
-  }
-}
-    `;
+    ${StandardUserFragmentDoc}
+`;
 
 export function useRegisterMutation() {
-  return Urql.useMutation<RegisterMutation, RegisterMutationVariables>(RegisterDocument);
-};
+    return Urql.useMutation<RegisterMutation, RegisterMutationVariables>(
+        RegisterDocument
+    );
+}
+export const CurrentUserDocument = gql`
+    query CurrentUser {
+        currentUser {
+            ...StandardUser
+        }
+    }
+    ${StandardUserFragmentDoc}
+`;
+
+export function useCurrentUserQuery(
+    options: Omit<Urql.UseQueryArgs<CurrentUserQueryVariables>, 'query'> = {}
+) {
+    return Urql.useQuery<CurrentUserQuery>({
+        query: CurrentUserDocument,
+        ...options,
+    });
+}
