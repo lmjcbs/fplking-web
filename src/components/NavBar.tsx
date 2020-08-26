e0d346f33f80c936b57cf8f2d9bbab6a91205801
lmjcbs <hello@liamjacobs.dev>
2020-08-26 22:48:14 +0100

    add navbar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Flex, Box, Link, Button } from '@chakra-ui/core';
+import NextLink from 'next/link';
+import { useCurrentUserQuery } from '../generated/graphql';
+
+interface NavBarProps {}
+
+export const NavBar: React.FC<NavBarProps> = ({}) => {
+    const [{ data, fetching }] = useCurrentUserQuery();
+    let body = null;
+    console.log(data);
+    if (!data?.currentUser) {
+        body = (
+            <>
+                <NextLink href='/login'>
+                    <Link mr={2}>Login</Link>
+                </NextLink>
+                <NextLink href='/register'>
+                    <Link>Register</Link>
+                </NextLink>
+            </>
+        );
+    } else {
+        body = (
+            <Flex>
+                <Box mr={2}>{data.currentUser.username}</Box>
+                <Button variant='link' color='black'>
+                    Logout
+                </Button>
+            </Flex>
+        );
+    }
+    return (
+        <Flex bg='grey' p={4}>
+            <Box ml={'auto'}>{body}</Box>
+        </Flex>
+    );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,10 @@
-const Index = () => (
-  <div>Hello World</div>
-)
+import { NavBar } from '../components/NavBar';
 
-export default Index
+const Index = () => (
+    <>
+        <NavBar></NavBar>
+        <div>Hello World</div>
+    </>
+);
+
+export default Index;
