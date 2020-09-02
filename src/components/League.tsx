c3aa034f91e698f1f6a2fabb42e794b45abcd071
lmjcbs <hello@liamjacobs.dev>
2020-09-02 21:41:33 +0100

    add league component wrapper

---
+++ src/components/League.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Box } from '@chakra-ui/core';
+
+interface LeagueProps {}
+
+export const League: React.FC<LeagueProps> = () => {
+    return <Box mt={8} mx='auto' w='100%'></Box>;
+};
